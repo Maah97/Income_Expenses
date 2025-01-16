559c35d64aac2799d54507f987823585dc2c9dd1
Maah97 <mahmoudouaboul@gmail.com>
2025-01-16 11:17:45 +0100

    add home section

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import './styles/app.scss'
 import Header from "./components/Header.jsx"
 import Footer from './components/Footer.jsx'
+import Home from './components/home.jsx'
 
 function App() {
 
   return (
     <>
       <Header />
+      <Home />
       <Footer />
     </>
   )
---
+++ src/components/home.jsx
@@ -0,0 +1,26 @@
+import { useEffect, useRef } from "react"
+
+export default function Home() {
+    let slogans = ["Track Earnings, Control Spending, with IncomesExpenses", "Master Your Money Journey with IncomesExpenses", "Track, Plan, and Prosper with IncomesExpenses", "Made Your Financial Roadmap Easy with IncomesExpenses", "Made Your Monthly Budget Simple with IncomesExpenses"]
+    const slogan = useRef(null)
+    useEffect(() => {
+        const txtSlogan = slogan.current
+        let i = 1
+        setInterval(() => {
+            txtSlogan.textContent = slogans[i]
+            i++
+            if (i === slogans.length) {
+                i = 0
+            }
+        }, 2000)
+        
+        
+    }, [])
+    return (
+        <section className="home">
+            <p>Record <span>income</span> Money </p>
+            <p>And <span>spent</span> Money </p>
+            <p ref={slogan} className="slogan">Track Earnings, Control Spending, with IncomesExpenses</p>
+        </section>
+    )
+}
