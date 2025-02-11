6fbe97f120c28d751a646cafb79c5eb88012930f
Maah97 <mahmoudouaboul@gmail.com>
2025-02-11 01:14:49 +0100

    finishing of the home page

---
+++ src/components/caroussel.jsx
@@ -0,0 +1,80 @@
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import img from "../assets/Tracking-Income-Expenses.webp"
+import img1 from "../assets/Budget-Planning.webp"
+import img2 from "../assets/Financial-Insights-Reports.webp"
+import img3 from "../assets/Saving-Investment-Planning.webp"
+import img4 from "../assets/Debt-Bill-Management.webp"
+import img5 from "../assets/Multi-Month-Long-Term-Financial-Analysis.webp"
+import img6 from "../assets/Personal-Business-Use.webp"
+
+
+
+export default function Caroussel() {
+    const settings = {
+        dots: true, // Affiche les points de navigation
+        infinite: true, // Défilement infini
+        speed: 500, // Vitesse de transition (ms)
+        slidesToShow: 1, // Nombre de slides visibles
+        slidesToScroll: 1, // Nombre de slides à faire défiler
+        autoplay: true, // Défilement automatique
+        autoplaySpeed: 4000, // Vitesse de l'auto-scroll (ms)
+    };
+    return (
+        <div className="carrousel-container">
+            <h3><span>Incomes</span><span>Expenses</span> can be used for</h3>
+            <Slider {...settings}>
+                <div>
+                    <img src={img} alt="Slide 1" />
+                    <div className="txt">
+                        <h4>Tracking Income and Expenses</h4>
+                        <p>Monitor all your earnings and spending in one place. Categorize transactions for better clarity.</p>
+                    </div>
+                </div>
+                <div>
+                    <img src={img1} alt="Slide 2" />
+                    <div className="txt">
+                        <h4>Budget Planning</h4>
+                        <p>Set monthly or yearly budgets to control spending. eceive alerts when you approach your budget limits.</p>
+                    </div>
+                </div>
+                <div>
+                    <img src={img2} alt="Slide 3" />
+                    <div className="txt">
+                        <h4>Financial Insights and Reports</h4>
+                        <p>Get detailed reports on income vs. expenses. Identify spending patterns and optimize finances.</p>
+                    </div>
+                </div>
+                <div>
+                    <img src={img3} alt="Slide 1" />
+                    <div className="txt">
+                        <h4>Saving and Investment Planning</h4>
+                        <p>Plan savings goals based on your income and expenses. Allocate funds for future investments.</p>
+                    </div>
+                </div>
+                <div>
+                    <img src={img4} alt="Slide 2" />
+                    <div className="txt">
+                        <h4>Debt and Bill Management</h4>
+                        <p>Keep track of recurring bills and payments. Avoid late fees by setting payment reminders.</p>
+                    </div>
+                </div>
+                <div>
+                    <img src={img5} alt="Slide 3" />
+                    <div className="txt">
+                        <h4>Multi-Month and Long-Term Financial Analysis</h4>
+                        <p>Analyze financial trends over months or years. Make informed decisions for financial stability.</p>
+                    </div>
+                </div>
+                <div>
+                    <img src={img6} alt="Slide 3" />
+                    <div className="txt">
+                        <h4>Personal and Business Use</h4>
+                        <p>Manage personal finances efficiently. Track business expenses if youC&apos;re self-employed.</p>
+                    </div>
+                </div>
+            </Slider>
+        </div>
+    )
+}
--- src/components/presentation.jsx
+++ src/components/presentation.jsx
@@ -1,4 +1,6 @@
 import { useEffect, useRef } from "react"
+import { NavLink } from "react-router-dom"
+import Caroussel from "./caroussel"
 
 export default function Presentation() {
     const slogan = useRef(null)
@@ -15,10 +17,54 @@ export default function Presentation() {
         }, 2000)
     }, [])
     return (
-        <section id="home" className="home">
-            <p>Record <span>income</span> Money </p>
-            <p>And <span>spent</span> Money </p>
-            <p ref={slogan} className="slogan">Track Earnings, Control Spending, with IncomesExpenses</p>
+        <section className="presentation">
+            <div id="home" className="home">
+                <p>Record <span>income</span> Money </p>
+                <p>And <span>expense</span> Money </p>
+                <p ref={slogan} className="slogan">Track Earnings, Control Spending, with IncomesExpenses</p>
+                <NavLink to="/signUp" className="btn-get-started">Get Started</NavLink>
+            </div>
+            <Caroussel />
+            <div className="why-income-expense">
+                <h3>Why <span>Incomes</span><span>Expenses</span> ?</h3>
+                <div className="container-answers">
+                    <div className="answer">
+                        <i className="fa-solid fa-square-poll-vertical"></i>
+                        <p>Simplified</p>
+                        <p>Simply add your income and expenses, track your budget, and get clear financial insights to manage your money wisely !</p>
+                    </div>
+                    <div className="answer">
+                        <i className="fa-solid fa-screwdriver-wrench"></i>
+                        <p>Smart Builder</p>
+                        <p>Monitor and optimize a smart building’s financial efficiency by tracking expenses, managing budgets, and analyzing cost trends in real time.</p>
+                    </div>
+                    <div className="answer">
+                        <i className="fa-solid fa-chart-column"></i>
+                        <p>Analyze the Results</p>
+                        <p>Visualize your financial trends with an interactive income vs. expenses graph, making it easy to analyze results, identify patterns, and optimize your budget.</p>
+                    </div>
+                    <div className="answer">
+                        <i className="fa-solid fa-bars"></i>
+                        <p>Customization</p>
+                        <p>Personalize your experience with customizable settings, including dark and light mode, for a seamless and comfortable financial tracking experience.</p>
+                    </div>
+                    <div className="answer">
+                        <i className="fa-solid fa-language"></i>
+                        <p>Two Language Support</p>
+                        <p>Effortlessly manage your finances with multilingual support, offering both French and English the world&apos;s most widely spoken international languages.</p>
+                    </div>
+                    <div className="answer">
+                        <i className="fa-solid fa-headset"></i>
+                        <p>Customer Support</p>
+                        <p>Get seamless assistance through our dedicated customer support, featuring a contact form for quick and efficient responses to your financial tracking inquiries.</p>
+                    </div>
+                    <div className="answer">
+                        <i className="fa-solid fa-user-lock"></i>
+                        <p>Security and Privacy</p>
+                        <p>Ensure your financial data stays secure with login/logout functionality, email verification, and password protection for enhanced privacy and peace of mind</p>
+                    </div>
+                </div>
+            </div>
         </section>
     )
 }
