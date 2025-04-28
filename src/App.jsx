172bf955b28796bf4f1c50dc590dc229f27e5210
Maah97 <mahmoudouaboul@gmail.com>
2025-04-28 22:44:45 +0100

    feat: extend loader visibility duration and enhance loader styles for improved user experience

--- src/App.jsx
+++ src/App.jsx
@@ -32,7 +32,7 @@ function App() {
     const timer = setTimeout(() => {
       setIsLoading(false);
       NProgress.done();
-    }, 500); // petite pause pour que le loader soit visible même si la page est rapide
+    }, 2000); // petite pause pour que le loader soit visible même si la page est rapide
 
     return () => clearTimeout(timer);
   }, [location.pathname]);
