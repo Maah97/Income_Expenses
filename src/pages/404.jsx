d575a7d6ec452017b95a494d8ecc93a28a834c86
Maah97 <mahmoudouaboul@gmail.com>
2025-04-28 22:09:26 +0100

    feat: add error page translations and update 404 component to use them

--- src/pages/404.jsx
+++ src/pages/404.jsx
@@ -1,12 +1,14 @@
 import { NavLink } from "react-router-dom"
+import { useTranslation } from "react-i18next"
 
 export default function ErrorPage() {
+    const { t } = useTranslation();
     return (
         <div className="error-page">
             <h1>404</h1>
-            <h2>Page Not Found</h2>
-            <p>The page you are looking for does not exist.</p>
-            <NavLink className="link-home" to="/">Go back to Home</NavLink>
+            <h2>{t("errorPage.h2")}</h2>
+            <p>{t("errorPage.p")}</p>
+            <NavLink className="link-home" to="/">{t("errorPage.home")}</NavLink>
         </div>
     )
 }
